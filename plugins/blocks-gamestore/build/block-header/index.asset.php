bc19fafd05620a1f92ad8ef0098dcad4cb2d082d
Bogdan Shmarov <smarovbogdan1@gmail.com>
2025-04-12 03:29:17 +0300

    final checks

--- plugins/blocks-gamestore/build/block-header/index.asset.php
+++ plugins/blocks-gamestore/build/block-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '05eedc248ea20429289c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '1e795cd9d5ef84af0d2e');
