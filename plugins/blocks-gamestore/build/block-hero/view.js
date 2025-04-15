71b43e5afcd34fd9f4f64bf12a90c9af21fdd0f3
Bogdan Shmarov <smarovbogdan1@gmail.com>
2025-04-15 03:11:56 +0300

    block hero ready

---
+++ plugins/blocks-gamestore/build/block-hero/view.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+/*!********************************!*\
+  !*** ./src/block-hero/view.js ***!
+  \********************************/
+document.addEventListener('DOMContentLoaded', function () {
+  const swiperHero = new Swiper('.hero-slider .slider-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    speed: 1500,
+    grabCursor: true,
+    mousewheelControl: true
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
