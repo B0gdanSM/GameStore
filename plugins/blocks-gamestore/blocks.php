d3217b16236e3dd299f20159a9d0978f426cadf4
Bogdan Shmarov <smarovbogdan1@gmail.com>
2025-05-01 05:05:15 +0300

    finishin block featured products

--- plugins/blocks-gamestore/blocks.php
+++ plugins/blocks-gamestore/blocks.php
@@ -95,3 +95,61 @@ function view_block_subscribe($attributes)
 	echo '</div>';
 	return ob_get_clean();
 }
+
+function view_block_featured_products($attributes)
+{
+	$featured_games = wc_get_products(array(
+		'status' => 'publish',
+		'limit' => $attributes['count'],
+		'featured' => true,
+		'orderby' => 'date',
+		'order' => 'DESC',
+	));
+
+	ob_start();
+	echo '<div ' . get_block_wrapper_attributes(array('class' => 'featured-products')) . '>';
+
+	if ($attributes['title']) {
+		echo '<h2>' . esc_html($attributes['title']) . '</h2>';
+	}
+	if ($attributes['description']) {
+		echo '<p>' . esc_html($attributes['description']) . '</p>';
+	}
+
+	$platforms = array('Xbox', 'PC', 'PlayStation');
+
+	if (!empty($featured_games)) {
+		echo '<div class="game-list">';
+		foreach ($featured_games as $product) {
+			$platforms_html = '';
+			
+			echo '<div class="game-result">';
+			echo '<a href="' . esc_url($product->get_permalink()) . '">';
+			echo '<div class="game-featured-image">' . $product->get_image('medium') . '</div>';
+			echo '<div class="game-meta">';
+			echo '<div class="game-price">' . $product->get_price_html() . '</div>';
+			echo '<h3 class="game-title">' . esc_html($product->get_name()) . '</h3>';
+			echo '<div class="game-platforms">';
+			foreach ($platforms as $platform) {
+				$meta_key = '_platform_' . strtolower($platform);
+				$class_name = 'platform-' . strtolower($platform);
+
+				if (get_post_meta($product->get_ID(), $meta_key, true) === 'yes') {
+					$platforms_html .= '<div class="' . esc_attr($class_name) . '"></div>';
+				}
+			}
+
+			echo $platforms_html;
+			echo '</div>';
+			echo '</div>';
+			echo '</a>';
+			echo '</div>';
+		}
+		echo '</div>';
+	} else {
+		echo '<p>No products found.</p>';
+	}
+
+	echo '</div>';
+	return ob_get_clean();
+}
