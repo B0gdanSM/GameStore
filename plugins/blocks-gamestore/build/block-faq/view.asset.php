2e5dcfcae8572b603bdc141eeb9a270c9135db56
Bogdan Shmarov <smarovbogdan1@gmail.com>
2025-05-09 03:27:59 +0300

    finised block-faq

---
+++ plugins/blocks-gamestore/build/block-faq/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1ddb92471457c02c5335');
---
+++ plugins/blocks-gamestore/build/block-faq/view.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+/*!*******************************!*\
+  !*** ./src/block-faq/view.js ***!
+  \*******************************/
+document.addEventListener('DOMContentLoaded', function () {
+  const faqItems = document.querySelectorAll('.faq-item');
+  faqItems.forEach(faq => {
+    const question = faq.querySelector('.faq-item-title');
+    const answer = faq.querySelector('.faq-item-description');
+    question.addEventListener('click', () => {
+      answer.classList.toggle('show');
+      question.classList.toggle('open');
+    });
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=view.js.map
